55fdd4eb0d142e853ed5e195d02eaa03522ffe5d
unknown <jadsonkenard@hotmail.comgit config --global user.email jadsonkenard@hotmail.com>
2024-09-07 13:02:44 -0300

    criado a tela home e o header

--- App.tsx
+++ App.tsx
@@ -1,49 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 import { Loading } from "./src/components";
-import { theme } from "./src/theme";
 import { Home } from "./src/screens";
 import { useEffect, useState } from "react";
+import { theme } from "./src/theme";
 
 export default function App() {
-  const[splash, setSplash] = useState(false)
+  const [splash, setSplash] = useState(false);
   const [fontLoad] = useFonts({
     Poppins_400Regular,
     Poppins_700Bold,
   });
 
-  function splashScreen(){
-    setSplash(true)
+  function splashScreen() {
+    setSplash(true);
   }
   useEffect(() => {
-    setTimeout(splashScreen, 3000)
-  },[])
+    setTimeout(splashScreen, 3000);
+  }, []);
 
   if (!fontLoad) {
-    return <Loading/>
+    return <Loading />;
   }
   return (
-    <>
-    {splash ? <Home/> : <Loading/>}
-    </>
-
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar
+        translucent
+        backgroundColor={theme.colors.primary}
+        barStyle="light-content"
+      />
+      {splash ? <Home /> : <Loading />}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontFamily: theme.fonts.regular,
-    fontSize: 25,
-  },
-});
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
+import { theme } from "../../theme";
+import user from "../../assets/user.png"
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <View style={styles.header}>
+        <View style={styles.infoUser}>
+          <Image source={user} style={styles.avatar}/>
+          <Text style={styles.userName}>Joao</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -11,10 +19,27 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  title: {
-    color: "red",
+  header: {
+    height: RFPercentage(18),
+    width: RFPercentage(100),
+    backgroundColor: theme.colors.primary
+  },
+  infoUser:{
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: RFPercentage(5),
+    marginLeft: RFPercentage(2),
+  },
+  avatar: {
+    height: RFPercentage(5),
+    width: RFPercentage(5),
+    borderRadius: 50
+  },
+  userName: {
+    fontFamily: theme.fonts.regular,
+    fontSize: 18,
+    color: theme.colors.primaryWhite,
+    marginLeft: RFPercentage(2),
   },
 });

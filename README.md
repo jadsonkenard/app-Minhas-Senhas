6099841cfadc46b7fa1199be62c4dd65705f10bb
unknown <jadsonkenard@hotmail.comgit config --global user.email jadsonkenard@hotmail.com>
2024-10-02 13:39:04 -0300

    Carregamento de senhas na tela de backup

--- README.md
+++
@@ -1,5 +0,0 @@
-REFATORAR TELA DE PROFILE
-
-  const nameString = dataUserName.map((item) => item.name).toString();
-  const lastNameString = dataUserName.map((item) => item.lastName).toString();
-  const emailString = dataUserName.map((item) => item.email).toString();
--- src/screens/backup/index.tsx
+++ src/screens/backup/index.tsx
@@ -1,8 +1,32 @@
 import { View, StyleSheet, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Button } from "../../components";
+import { useState } from "react";
+
+const KEY_STORAGE = "@appmypass";
+
+type Data = {
+  id: string;
+  nameApp: string;
+  loginApp: string;
+  passwordApp: string;
+};
 
 export function Backup() {
+  const [backup, setBackup] = useState<Data[]>([])
+
+  async function getPasswordsBackup(){
+    const response = await AsyncStorage.getItem(KEY_STORAGE);
+    const dataBackup = response ? JSON.parse(response) : [];
+
+    setBackup(dataBackup)
+    // console.log(setBackup)
+
+  }
+
   return <View style={styles.container}>
-    <Text>Senhas</Text>
+    <Button onPress={getPasswordsBackup}><Text>Ver</Text></Button>
+    <Text>{backup.map((item) => item.passwordApp)}</Text>
   </View>;
 }
 
